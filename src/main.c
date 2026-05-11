63ba402de08621715009147661c06a1e9bc06f6e
n0sferatu09 <noki4ngelvim@gmail.com>
2026-05-11 14:18:41 +0900

    update main.c for command parse (beta)

--- src/main.c
+++ src/main.c
@@ -10,31 +10,56 @@
 #include "ast/ast.h"
 
 int main(int argc, char **argv) {
-    parse_flags(argc, argv);
+    Flags *flags = parse_flags(argc, argv);
+    if (!flags) return 1;
 
-    FILE *file = fopen("../syntax.hm", "r");
+    FILE *file = fopen(flags->input_filename, "r");
     if (!file) {
         perror("fopen");
+        free_flags(flags);
         return 1;
     }
 
-    FILE *output = fopen("../test.c", "w");
+    char output_name[256];
+    strcpy(output_name, flags->input_filename);
+    char *dot = strchr(output_name, '.');
+    if (dot) *dot = '\0';
+    strcat(output_name, ".c");
+
+    FILE *output = fopen(output_name, "w");
     if (!output) {
         perror("fopen");
+        fclose(file);
+        free_flags(flags);
         return 1;
     }
 
     ASTNode *ast = parser(file);
-
     SymbolTable *symbol_table = create_symbol_table();
-
     bool ok = semantic_analyze(ast, symbol_table);
 
-    free_symbol_table(symbol_table);
+    if (ok) {
+        codegen(ast, output, 0);
+    }
 
-    codegen(ast, output, 0);
+    if (flags->compile_flag && ok) {
+        char cmd[512];
+        const char *cc =
+            flags->cc == CC_GCC ? "gcc" :
+            flags->cc == CC_GPP ? "g++" :
+            flags->cc == CC_CLANG ? "clang" : "clang++";
+
+        sprintf(cmd, "%s %s -o %s", cc, output_name, flags->input_filename);
+        dot = strchr(cmd, '.');
+        if (dot) *dot = '\0';
+
+        system(cmd);
+    }
 
     fclose(file);
     fclose(output);
+    free_symbol_table(symbol_table);
+    free_flags(flags);
+    free_ast(ast);
     return 0;
 }
